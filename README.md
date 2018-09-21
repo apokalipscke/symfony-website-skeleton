68eba38ee3f3daae0c85a37b074cf1bcba179732
Marc Lipscke <m.lipscke@markenstuermer.de>
2018-09-21 23:38:44 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# Symfony 4 Website Skeleton
+
+## Contents
+
+### Backend
+
+- symfony
+- symfony/orm-pack
+- symfony/security-bundle
+- symfony/web-server-bundle
+- symfony/webpack-encore-pack
+
+### Frontend
+
+- fontawesome-free v5
+- jquery v3
+- bootstrap v4
+- mdbootstrap v4
+
+## Usage
+
+```
+composer install
+npm install
+npm run watch
+```
---
+++ assets/js/app.js
@@ -0,0 +1,19 @@
+// let $ = require('jquery');
+// require('bootstrap');
+// require('mdbootstrap');
+// require('../css/app.scss');
+
+// jQuery
+import 'jquery';
+// PopperJS
+import 'popper.js';
+// Bootstrap 4
+import 'bootstrap';
+// Material Design Bootstrap
+// import '../vendors/mdb/js/mdb';
+import 'mdbootstrap/js/mdb';
+require('../css/app.scss');
+
+$(document).ready(function () {
+    console.log("ready!");
+});
